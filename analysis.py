40429653dd2e55800668b640a657c9fda20d1277
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2020-04-15 12:39:46 -0400

    comment edits

--- analysis.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-reader = csv.DictReader(open('scrape.csv', 'r'))
-result = sorted(reader, key=lambda d: d['author'])
-
-writer = csv.DictWriter(open('analysis.csv', 'w'), reader.fieldnames)
-writer.writeheader()
-writer.writerows(result)
---
+++ sort_scrape.py
@@ -0,0 +1,16 @@
+# SORT SCRAPE
+
+# Dependenices
+import csv
+
+# Create object that maps rows to a dictionary.
+reader = csv.DictReader(open('scrape.csv', 'r'))
+# Sort rows in object by key name 'author' via lambda function.
+result = sorted(reader, key=lambda d: d['author'])
+
+# Create object that maps dictionaries onto output rows.
+writer = csv.DictWriter(open('analysis.csv', 'w'), reader.fieldnames)
+# Write row with assigned fieldnames in 'analysis.csv'.
+writer.writeheader()
+# Write newly sorted rows to output file.
+writer.writerows(result)
