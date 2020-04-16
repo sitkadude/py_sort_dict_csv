a48d0d31aa36c52b07e38bc82c9b0080bc60c54f
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2020-04-15 22:23:48 -0400

    edited print function in py file

--- sort_scrape.py
+++ sort_scrape.py
@@ -3,14 +3,17 @@
 # Dependenices
 import csv
 
+print("Sorting csv file...")
 # Create object that maps rows to a dictionary.
 reader = csv.DictReader(open('scrape.csv', 'r'))
 # Sort rows in object by key name 'author' via lambda function.
 result = sorted(reader, key=lambda d: d['author'])
 
 # Create object that maps dictionaries onto output rows.
-writer = csv.DictWriter(open('analysis.csv', 'w'), reader.fieldnames)
+writer = csv.DictWriter(open('sorted_scrape.csv', 'w'), reader.fieldnames)
 # Write row with assigned fieldnames in 'analysis.csv'.
 writer.writeheader()
 # Write newly sorted rows to output file.
 writer.writerows(result)
+
+print("Sort complete!")
